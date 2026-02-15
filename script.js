8c7d7a5c84511920dc98a5c0a07171d33dbaad92
centralperk531-bit <centralperk.531@gmail.com>
2026-02-15 12:10:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1102,10 +1102,14 @@ async function verificarPassword(event) {
         
         await cargarDatosGoogle();
         generarCalendario();
-               // PASO 1: Ocultar listado de paquetes al entrar como admin
-        if (document.getElementById('listadoPaquetes')) {
-            document.getElementById('listadoPaquetes').style.display = 'none';
-        }
+             // PASO 1: Ocultar listado de paquetes al entrar como admin
+        document.addEventListener('DOMContentLoaded', function() {
+            const listado = document.getElementById('listadoPaquetes');
+            if (listado) {
+                listado.style.display = 'none';
+            }
+        });
+
                 // PASO 2: Ocultar descripción larga del header
         if (document.getElementById('headerDescripcionLarga')) {
             document.getElementById('headerDescripcionLarga').style.display = 'none';
